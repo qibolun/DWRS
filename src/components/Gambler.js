034b719170fb78b425496ac039c644c24e997c88
Bolun <bolunqi2@illinois.edu>
2018-02-22 20:04:20 -0600

    add quit game

--- src/components/Gambler.js
+++ src/components/Gambler.js
@@ -7,6 +7,7 @@ class Gambler extends React.Component{
 		super(props);
 		this.state = {}
 		this.joinGame = this.joinGame.bind(this)
+		this.quitGame = this.quitGame.bind(this)
 	}
 
 	joinGame(){
@@ -15,9 +16,23 @@ class Gambler extends React.Component{
 			from: account,
 			value: web3.toWei(1)
 		}).then((result) => {
+			console.log("Game joined")
 			console.log(result)
 		}).catch(() => {
-			console.log("error!")
+			console.log("joinGame error!")
+		})
+	}
+
+	quitGame(){
+		const { gamble, account } = this.props
+		gamble.quitGame({
+			from:account
+		}).then((result) => {
+			console.log("Game quitted")
+			console.log(result)
+		}).catch((error) => {
+			console.log("quitGame error")
+			console.log(error)
 		})
 	}
 
@@ -26,6 +41,7 @@ class Gambler extends React.Component{
 			<div>
 				This is gambler Component
 				<Button onClick={this.joinGame}>Join Game</Button>
+				<Button onClick={this.quitGame}>Quit Game</Button>
 			</div>
 		)
 	}
